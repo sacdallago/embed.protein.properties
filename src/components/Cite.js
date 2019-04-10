50efd4f097f40abd911345628613015ac31aef5d
Christian Dallago <code@dallago.us>
2019-04-10 15:22:52 +0200

    pre formatting

--- src/components/Cite.js
+++ src/components/Cite.js
@@ -8,7 +8,9 @@ const styles = theme => ({
     root: {
         overflowX: 'auto',
         textAlign: "center",
-        paddingBottom: theme.spacing.unit
+        paddingBottom: theme.spacing.unit,
+        paddingLeft: theme.spacing.unit*5,
+        paddingRight: theme.spacing.unit*5
     },
     underline: {
         textDecoration: "underline",
@@ -34,7 +36,7 @@ class Cite extends React.Component {
                     Cite
                 </Typography>
                 <pre>
-                    SOMEPAPER
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultrices neque eget lorem commodo, quis feugiat mi finibus. Fusce blandit porttitor porta. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed erat arcu, consectetur id ornare et, eleifend id urna. Integer accumsan fermentum nibh, ut rutrum purus lobortis quis. Aliquam iaculis mauris id viverra rutrum. Mauris lacinia mi dui, at tempus purus vestibulum sit amet. Morbi malesuada id neque et imperdiet. Nullam non urna sit amet urna efficitur volutpat vitae vel nisi. Suspendisse potenti. Aenean volutpat sagittis quam, at gravida purus accumsan et. Morbi cursus sodales pulvinar. Phasellus id odio in neque condimentum ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod, ex fermentum tempus pretium, leo leo sodales risus, et lobortis nibh lectus a nunc. Duis erat ligula, porta vitae dui ut, lacinia consequat ex.
                 </pre>
             </Paper>
         );
