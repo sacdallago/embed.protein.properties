4714f183d623bef6197726392a0accbcd5f0a82b
Christian Dallago <code@dallago.us>
2019-04-11 23:28:30 +0200

    Updates

---
+++ precache-manifest.99646a3728faf9205b87630455ec75ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6d55d08fc3ae14ac5de",
+    "url": "/static/css/main.01b39466.chunk.css"
+  },
+  {
+    "revision": "c6d55d08fc3ae14ac5de",
+    "url": "/static/js/main.3262f18c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ed5c4d1f27a6d48b1f92",
+    "url": "/static/js/2.d6320bda.chunk.js"
+  },
+  {
+    "revision": "5f349f98427884d3e29665078b5d6673",
+    "url": "/index.html"
+  }
+];
